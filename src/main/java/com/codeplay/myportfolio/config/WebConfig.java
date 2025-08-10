2921c5ee1cfa2abde22950f94657cb5db6124710
shubhamk099 <shubhamkkumar9797@gmail.com>
2025-08-10 15:39:53 +0530

    added thymeleaf tags and controllers

---
+++ src/main/java/com/codeplay/myportfolio/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.codeplay.myportfolio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/projects").setViewName("projects");
+        registry.addViewController("/resume").setViewName("resume");
+    }
+}
---
+++ src/main/java/com/codeplay/myportfolio/controller/ContactController.java
@@ -0,0 +1,14 @@
+package com.codeplay.myportfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ContactController {
+
+    @RequestMapping(value = "/contact", method = RequestMethod.GET)
+    public String displayContactPage() {
+        return "contact";
+    }
+}
